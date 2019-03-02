3f99c33b0d6fd251692efb356989fe3237271ec7
Erick <erickluz360@gmail.com>
2019-03-02 16:29:14 -0300

    Alteracoes no ambiente de desenvolvimento

---
+++ src/main/java/com/erick/wallet/services/DBService.java
@@ -0,0 +1,12 @@
+package com.erick.wallet.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+	public void instanciacaoBancoDeDados() {
+		
+	}
+	
+}
