812458aea3822d4171ef989505d2ee12ad43dbc9
Erick <erickluz360@gmail.com>
2019-03-03 22:44:49 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/com/erick/wallet/config/DevConfig.java
@@ -0,0 +1,23 @@
+package com.erick.wallet.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.erick.wallet.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase(){
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
